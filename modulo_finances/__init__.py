53e188bc9cb6235634f2f6e59a6d9caf72b33811
Ap0lo-01 <limaapolo91@gmail.com>
2024-12-08 16:26:49 -0300

    Create __init__.py

---
+++ modulo_finances/__init__.py
@@ -0,0 +1,5 @@
+from .transaction import Transaction
+from .account import Account
+from .investment import Investment
+from .client import Client
+from .report import generate_report, future_value_report
