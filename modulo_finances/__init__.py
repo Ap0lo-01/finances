bc48e25cfc28bdabc16909e0a809ac9c04fca156
Ap0lo-01 <limaapolo91@gmail.com>
2024-12-08 19:13:25 -0300

    Update __init__.py

--- modulo_finances/__init__.py
+++ modulo_finances/__init__.py
@@ -2,4 +2,3 @@ from .transaction import Transaction
 from .account import Account
 from .investment import Investment
 from .client import Client
-from .report import generate_report, future_value_report
