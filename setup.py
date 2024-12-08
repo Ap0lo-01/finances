0032d711265ba64bb25e05561791d2cab77b534c
Ap0lo-01 <limaapolo91@gmail.com>
2024-12-08 17:39:43 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='modulo_finances',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={},
+    author='Seu Nome',
+    author_email='seu_email@example.com',
+    description='Um módulo para gerenciar finanças pessoais',
+    license='MIT',
+    keywords='finanças pessoais transações investimentos',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
