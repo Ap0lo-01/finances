405f084075147a3bb36451be614d26d356ffec25
Ap0lo-01 <limaapolo91@gmail.com>
2024-12-08 21:24:03 -0300

    Update __init__.py

--- modulo_finances/__init__.py
+++ modulo_finances/__init__.py
@@ -1,4 +1,1 @@
-from .finances import Transaction
-from .finances import Account
-from .finances import Investment
-from .finances import Client
+from .finances import Transaction, Account, Investment, Client
